83e599749f618b059e78a113cd12a56ad521bfb4
Chris <chris.lloyd.hi@gmail.com>
2018-12-02 11:43:27 +1300

    Add easy service install for Pi

---
+++ install_service.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check Args
+if [ -z "$1" ] || [ $1 == "-h" ] || [ $1 == "--help" ]
+    then
+        echo "Usage: sudo install_service.sh /path/to/code"
+        exit
+fi
+
+# Create the service file
+echo "Creating Service File in /etc/systemd/system/ci-christmas-tree.service"
+echo "
+[Unit]
+Description=CI Christmas Tree
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=$1
+ExecStart=/bin/bash $1/run.sh
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ci-christmas-tree.service
+
+# Enable the service
+systemctl enable ci-christmas-tree
+
+# Start the service
+systemctl start ci-christmas-tree
+echo "Please hold... Service Starting..."
+
+# Delay here while the ci christmas tree starts (approx 35 sec)
+sleep 35s
